9b000caa4f64abe8bc569228de1477b3f6ad51ff
Aleisha Mork <aleisha.mork@gmail.com>
2022-09-26 11:55:29 -0600

    feat: creat sort by caseload method for doctors and apply to the hospitle show page.

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,10 @@
 class Hospital < ApplicationRecord
   has_many :doctors
+
+  def doctors_caseload_sort
+    doctors.joins(:doctor_patients)
+    .group(:id)
+    .select('doctors.*, sum(doctor_patients.patient_id) as patient_count')
+    .order(patient_count: :desc)
+  end
 end
